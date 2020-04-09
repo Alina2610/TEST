474e8e1fcc1a01d075af771f772c8dc8f41d66cd
annright34@gmail.com <annright34@gmail.com>
2020-04-09 16:24:28 +0300

    мой первый коммит

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace WpfApp1
 {
     /// <summary>
@@ -23,6 +29,44 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            OpenPage(pages.login);
+            DataTable dt_user = Select("SELECT * FROM [dbo].[users]"); // получаем данные из таблицы
+
+            for (int i = 0; i < dt_user.Rows.Count; i++)
+            { // перебираем данные
+                MessageBox.Show(dt_user.Rows[i][0] + "|" + dt_user.Rows[i][1]); // выводим данные
+            }
+
+
+
+
+
         }
+        public enum pages
+        {
+            login,
+            regin
+        }
+        public void OpenPage(pages pages)
+        {
+            if (pages == pages.login)
+            {
+                frame.Navigate(new login(this));
+            }
+            else if (pages == pages.regin)
+                frame.Navigate(new regin(this));
+        }
+        public DataTable Select(string selectSQL)
+        {
+            DataTable dataTable = new DataTable("dataBase");
+            SqlConnection sqlConnection = new SqlConnection("server=LAPTOP-VQ2U0OII;Trusted_Connection=Yes;DataBase=TEST;");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = selectSQL;
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            sqlDataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+
     }
 }
---
+++ WpfApp1/WpfApp1/login.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static WpfApp1.MainWindow;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для login.xaml
+    /// </summary>
+    public partial class login : Page
+    {
+        public MainWindow mainWindow;
+        public login(MainWindow _mainWindow)
+        {
+            InitializeComponent();
+            mainWindow = _mainWindow;
+        }
+
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox_login.Text.Length > 0) // проверяем введён ли логин     
+            {
+                if (password.Password.Length > 0) // проверяем введён ли пароль         
+                {             // ищем в базе данных пользователя с такими данными         
+                    DataTable dt_user = mainWindow.Select("SELECT * FROM [TEST].[dbo].[users] WHERE [login] = '" + textBox_login.Text + "' AND [password] = '" + password.Password + "'");
+                    if (dt_user.Rows.Count > 0) // если такая запись существует       
+                    {
+                        MessageBox.Show("Пользователь авторизовался"); // говорим, что авторизовался         
+                    }
+                    else MessageBox.Show("Пользователя не найден"); // выводим ошибку  
+                }
+                else MessageBox.Show("Введите пароль"); // выводим ошибку    
+            }
+            else MessageBox.Show("Введите логин"); // выводим ошибку 
+
+        }
+        private void regin_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.OpenPage(MainWindow.pages.regin);
+        }
+
+    }
+
+
+}
